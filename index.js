70593f3860e5fde75d8665ad71af4757cfb7b7a6
JAHANA SHIRIN K <143816535+jahanashirin09@users.noreply.github.com>
2024-02-25 16:50:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let Lbox=document.getElementById("llbox")
+let Rbox=document.getElementById("rlbox")
+let count=0
+let ccount=0
+function one(){
+count+=1
+Lbox.textContent=count
+
+}
+function oone(){
+    ccount+=1
+Rbox.textContent=ccount
+
+}
+function two(){
+    count+=2
+Lbox.textContent=count
+
+}
+function ttwo(){
+    ccount+=2
+Rbox.textContent=ccount
+
+}
+function three(){
+    count+=3
+Lbox.textContent=count
+
+}
+function tthree(){
+    ccount+=3
+Rbox.textContent=ccount
+
+}
